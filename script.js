9b02d94091139ca561cd69e32834957158d8198d
rudy <cryptographycipher29@gmail.com>
2023-12-09 16:42:28 +0545

    Implement game logic

---
+++ script.js
@@ -0,0 +1,82 @@
+const elements = {
+    header            : document.querySelector("header"),
+    playerScore       : document.querySelector(".human-score"),
+    computerScore     : document.querySelector(".computer-score"),
+    playerWeaponImage : document.querySelector(".human > .rest"),
+    AIWeaponImage     : document.querySelector(".computer > .rest"),
+    weapons           : document.querySelector(".weapons"),
+    winMessageField   : document.querySelector(".win-message")
+};
+
+const weaponImages = {
+    "rest"    : "./images/rest.png",
+    "rock"    : "./images/rock.png",
+    "paper"   : "./images/paper.png",
+    "scissor" : "./images/scissors.png"
+}
+
+function AIWeaponChoice() {
+    return [Math.floor(3 * Math.random())]
+}
+
+function increasePlayerScore() {
+    elements.playerScore.textContent = 
+        String(parseInt(elements.playerScore.textContent)+1);
+}
+
+function increaseAIScore() {
+    elements.computerScore.textContent = 
+        String(parseInt(elements.computerScore.textContent)+1); 
+}
+
+function determineWinner(playerChoice, AIChoice) {
+    const winnerResult = (3 + playerChoice - AIChoice) % 3;
+    if (winnerResult === 2)
+        increaseAIScore();
+    else if (winnerResult === 1)
+        increasePlayerScore();
+    displayWinMessage(winnerResult);
+}
+
+function displayWinMessage(winner) {
+    let msg;
+    if (winner == 1)
+        msg = "Player Won" 
+    else if (winner == 2)
+        msg = "Computer Won"
+    else
+        msg = "Its Tie"
+    elements.winMessageField.textContent = `${msg}`;
+    setTimeout( () => {
+        elements.winMessageField.textContent = "";
+    }, 1500 )
+}
+
+for (let i = 0; i < elements.weapons.children.length; i++) {
+    const weapon = elements.weapons.children[i];
+    weapon.addEventListener('click', (e) => {
+        const AIWeapon = AIWeaponChoice();
+        const AIweaponName = 
+            elements.weapons.children[AIWeapon].textContent.toLocaleLowerCase()
+
+        setTimeout( () => {
+            elements.playerWeaponImage.classList.toggle("rest");
+            elements.AIWeaponImage.classList.toggle("rest");
+
+            elements.playerWeaponImage.src = 
+                weaponImages[weapon.textContent.toLocaleLowerCase()];
+            elements.AIWeaponImage.src = weaponImages[AIweaponName]
+                
+
+        } , 200 )
+
+        setTimeout( () => {
+            elements.playerWeaponImage.classList.toggle("rest");
+            elements.AIWeaponImage.classList.toggle("rest");
+            elements.playerWeaponImage.src = weaponImages["rest"]        
+            elements.AIWeaponImage.src = weaponImages["rest"]   
+        }, 1500 )
+
+        determineWinner(i, AIWeapon)
+    })
+}
